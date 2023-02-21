e5a263f29336eefe44e69ca33ff273828bf12985
Baptiste LABAUNE <baptiste.labaune@gmail.com>
2023-02-21 15:51:24 +0100

    second page generation + modal work

--- scripts/factories/photographerProfile.js
+++ scripts/factories/photographerProfile.js
@@ -1,69 +1,64 @@
 //factory pattern - fonction entière
-function photographerFactory(data) {
+function displayProfile(data) {
+
+    const profileData = data[1];
     
-    const { name, portrait, city, country, tagline, price, id } = data; // => name = data.name & portrait = data.portrait --> nouvel syntaxe es6
-    const picture = `../../assets/photographers/Sample Photos/Photographers ID Photos/${portrait}`; //portrait = data.portrait
+    const photographersHeader = document.querySelector(".photograph-header");
+    const { name, portrait, city, country, tagline, price, id } = profileData; 
+    const picture = `./assets/photographers/Sample Photos/Photographers ID Photos/${portrait}`;
 
-    //fonction de modification du DOM = création de la card portrait
-    function getUserCardDOM() {
+    //creation du div avec nom/city/country/tagline
+    const infos = document.createElement('div');
 
-        //Création de l'élement parent
-        const article = document.createElement( 'article' );
+    //Name
+    const photographerName = document.createElement( 'h1' );
+    photographerName.textContent = name;
+
+    //City + country
+    const photographerLocation = document.createElement('p');
+    photographerLocation.innerText = `${city}, ${country}`;
+
+    //tagline
+    const photographerTagline = document.createElement('p');
+    photographerTagline.innerText = tagline;
+
+    //Append to the block
+    infos.appendChild(photographerName);
+    infos.appendChild(photographerLocation);
+    infos.appendChild(photographerTagline);
 
         //Img
-        const photographersPicture = document.createElement( 'img' );
-        photographersPicture.setAttribute("src", picture)
-        photographersPicture.setAttribute("alt", '' )
-        
-
-        //Name
-        const photographerName = document.createElement( 'h2' );
-        photographerName.textContent = name;
-        
-
-        //IMG + NAME link
-        const photographerLink = document.createElement('a');
-        photographerLink.setAttribute('href', '#');
-        photographerLink.setAttribute('aria-label', `${name} - Profil`)
-        photographerLink.appendChild(photographersPicture);
-        photographerLink.appendChild(photographerName);
-        article.appendChild(photographerLink);
-        
+    const photographersPicture = document.createElement( 'img' );
+    photographersPicture.setAttribute("src", picture)
+    photographersPicture.setAttribute("alt", '' )
 
 
-        //Country + City
-        const photographerLocation = document.createElement('p');
-        photographerLocation.innerText = `${city}, ${country}`;
-        article.appendChild(photographerLocation);
-        photographerLocation.classList.add('location');
+    //Append to the DOM
+    photographersHeader.prepend(infos);
+    photographersHeader.appendChild(photographersPicture);
 
-        //Tagline
-        const photographerTagline = document.createElement('p');
-        photographerTagline.innerText = tagline;
-        article.appendChild(photographerTagline);
-        photographerTagline.classList.add('tagline');
-    
+
+    //Country + City
+    photographerLocation.classList.add('location');
+
+    //Tagline
+    photographerTagline.classList.add('tagline');
 
         //Price
-        const photographerPrice = document.createElement('p');
+       /*  const photographerPrice = document.createElement('p');
         photographerPrice.innerText = `${price}€/jours`;
         article.appendChild(photographerPrice);
-        photographerPrice.classList.add('price');
+        photographerPrice.classList.add('price'); */
 
         //ID
-        article.setAttribute('id', id)
+        /* article.setAttribute('id', id) */
 
 
         //Creation du lien de redirection 
-        article.addEventListener('click', (event) =>{
+        /* article.addEventListener('click', (event) =>{
             let urlProfile = `../../photographer.html?id=${id}`;
             photographerLink.setAttribute('href', urlProfile)
-        })
+        }) */
 
-
-        return (article);
     }
 
-    //renvoi un objet à la fin !
-    return { name, picture, getUserCardDOM }
-}
--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -1,65 +1,42 @@
-                            //PAGE PROFIL//
+ 
+    //Fonctions qui récupère les données
+    async function getPhotographers() {
+        const photographers = await fetch('./data/photographers.json');   
+        if (photographers.ok) {
+            const data = await photographers.json();
+            const params = new URLSearchParams(window.location.search);
+            const idProfile = params.get('id');
 
-//Fonctions qui récupère les données
-/* async function getPhotographers() {
-    let photographers = await fetch('../../data/photographers.json');   
-    if(photographers.ok) {
-        let data = photographers.json();
-        filterData(data);
-    }
-    else {
-        alert('Les données n\'ont pas pu être récupérées')
-    }
-} */
+            const filteredPhotographerData = data.photographers.filter((item) => {
+                return item.id == idProfile;
+            });   
+            const filteredMediaData = data.media.filter((item) => {
+                return item.photographerId == idProfile;
+            });
+    
+            return filteredData = [filteredMediaData, profileData = filteredPhotographerData[0]];
+            /* retourne un tableau avec :
+            [0] = données relatives au TRAVAIL du photographe
+            [1] = données relatives au PROFIL du photographe */
+
+        } else {
+          alert('Les données n\'ont pas pu être récupérées')
+        }
+      }
+      
 
 
-    //fonction qui affiche les données
-    async function displayData(photographers) {
-        //récupération de l'élement du DOM
-        const photographersSection = document.querySelector(".photograph-header");
-
-        //Pour chaque élement du tableau de données (1 élement = 1 objet = 1 profil)
-        photographers.forEach((photographer) => {
-            //l'objet renvoyé est assigné à une variable
-            const photographerModel = photographerFactory(photographer);
-            //on assigne à une variable le 3e element de l'objet --> la fonction de modif du DOM
-            const userCardDOM = photographerModel.getUserCardDOM();
-            //on modifie l'élement du DOM avec la nouvelle variable
-            photographersSection.appendChild(userCardDOM);
-        });
+    async function init() {
+        const profile = await getPhotographers();
+        displayProfile(profile);
+        displayMedia(profile);
     };
+    
+    init(); 
 
 
-  //fonction principale
-  async function init() {
-    // Récupère les datas des photographes
-    const { photographers } = await getPhotographers();
-    displayData(photographers);
-};
 
-init();
+        
 
 
-async function filterData() {
-    let photographers = await fetch('../../data/photographers.json');   
-    if(photographers.ok) {
-        let data = photographers.json();
-    }
-    else {
-        alert('Les données n\'ont pas pu être récupérées')
-    }
-
-    //récupération de tous les paramètres de l'URL
-    let params = (new URL(window.location)).searchParams;
-    //récupération de l'ID uniquement
-    let idProfile = params.get('id');
-    const result = data.filter( (idProfile, id) => 
-    { 
-        id === idProfile;
-    })
-}
-
-
-async function getPhotographers() {
-        filterData();
-    };
+    
